4f4c741a4755a13f81716e752930d8ea8e214027
dsill <daniel.sill@stonybrook.edu>
2015-10-12 18:51:30 -0400

    Added fields/operations to Appointment including basic status flow

--- src/models/Appointment.java
+++ src/models/Appointment.java
@@ -1,5 +1,44 @@
 package models;
 
-public class Appointment {
+import java.util.Date;
 
+import utils.Status;
+
+public class Appointment {
+	private String appointmentID;
+	private Student student;
+	private Date date;
+	private Exam exam;
+	private Status status;
+	
+	public void prepare(){
+		if(checkValid()){
+			status = Status.APPROVED;
+		} else {
+			status = Status.DENIED;
+		}
+		//TODO: stub
+	}
+	public void activate(){
+		if(status == Status.APPROVED){
+			status = Status.ACTIVE;
+		}
+		//TODO: stub
+	}
+	public boolean checkValid(){
+		//TODO: stub
+		return true;
+	}
+	public void checkInStudent(){
+		if(status == Status.ACTIVE){
+			status = Status.CHECKED_IN;
+		}
+		//TODO: stub
+	}
+	public void end(){
+		if(status == Status.ACTIVE || status == Status.CHECKED_IN){
+			status = Status.INACTIVE;
+		}
+		//TODO: stub
+	}
 }
