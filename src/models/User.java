47b2defea4a5b076f5f3cc0a984ac7aa8f8be147
dsill <daniel.sill@stonybrook.edu>
2015-10-12 16:53:34 -0400

    Added fields to User

--- src/models/User.java
+++ src/models/User.java
@@ -2,4 +2,88 @@ package models;
 
 public abstract class User {
 
+	private String netID;
+	private String password;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private Type type;
+	
+	
+	
+	public String getNetID() {
+		return netID;
+	}
+
+
+
+	public void setNetID(String netID) {
+		this.netID = netID;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public Type getType() {
+		return type;
+	}
+
+
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+
+
+	public enum Type{
+		ADMIN, INSTRUCTOR, STUDENT
+	}
 }
