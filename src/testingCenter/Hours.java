8249f07b88e3037370cce2a192e557f0af8866bc
dsill <daniel.sill@stonybrook.edu>
2015-10-12 19:12:38 -0400

    Added TestingCenter outline

---
+++ src/testingCenter/Hours.java
@@ -0,0 +1,8 @@
+package testingCenter;
+
+import java.util.Date;
+
+public class Hours {
+	Date startTime;
+	Date endTime;
+}
---
+++ src/testingCenter/Term.java
@@ -0,0 +1,10 @@
+package testingCenter;
+
+import java.util.Date;
+
+public class Term {
+	Date startDate;
+	Date endDate;
+	
+	Hours[] hours = new Hours[7];
+}
