f2defe853045ba4a8ac23be994393bba128e72c9
Haseeb Shahid <hshahid92@gmail.com>
2015-10-13 10:15:01 -0400

    Removed unused import

--- src/DBWorks/DBConnection.java
+++ src/DBWorks/DBConnection.java
@@ -9,7 +9,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
