90bf202dd3ec8dbc88a3230c6dab5795f7d34012
Haseeb Shahid <hshahid92@gmail.com>
2015-10-13 08:47:35 -0400

    The MySQL script now supports importing from csv files. I commented those queries though because you guys will have different paths to the csv files. You can uncomment those queries and change the path to match yours

--- web/sql/initdb.sql
+++ web/sql/initdb.sql
@@ -1,7 +1,9 @@
+/* Table for users of the system
+The 'UserType' field specified Instructor, Administrator, or Student */
 CREATE TABLE TCSUser (
 	NetId VARCHAR(20) NOT NULL,
 	Password VARCHAR(20),
-	Email VARCHAR(20),
+	Email VARCHAR(30),
 	FirstName VARCHAR(20),
 	LastName VARCHAR(20),
 	UserType CHAR(15),
@@ -9,6 +11,23 @@ CREATE TABLE TCSUser (
 	UNIQUE (Email)
 );
 
+/* Inserting dummy user into TCSUser table */
+INSERT INTO TCSUser (NetId, Password, Email, FirstName, LastName, UserType)
+VALUES ('jsmith', 'pass123', 'jsmith@stonybrook.edu', 'Joe', 'Smith', 'Student');
+
+/* Query for importing information from user.csv */
+/*LOAD DATA LOCAL INFILE 'C:/Users/Haseeb/Desktop/user.csv'
+INTO TABLE TCSUser
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(FirstName, LastName, NetID, Email)
+SET Password = 'pass';*/
+
+/* For testing, all users from user.csv are imported as Students */
+/*UPDATE TCSUser SET UserType = 'Student';*/
+
+/* Table for course information */
 CREATE TABLE Course (
 	Id CHAR(10) NOT NULL,
 	Subject CHAR(3),
@@ -20,6 +39,15 @@ CREATE TABLE Course (
 		REFERENCES TCSUser (NetId)
 );
 
+/* Query for importing information from class.csv */
+/*LOAD DATA LOCAL INFILE 'C:/Users/Haseeb/Desktop/class.csv'
+INTO TABLE Course
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(Id, Subject, CatalogNumber, Section, InstructorNetId);*/
+
+/* Table for roster information */
 CREATE TABLE Roster (
 	NetId VARCHAR(20) NOT NULL,
 	CourseID CHAR(10) NOT NULL,
@@ -31,8 +59,17 @@ CREATE TABLE Roster (
 		ON DELETE CASCADE
 );
 
+/* Query for importing information from roster.csv */
+/*LOAD DATA LOCAL INFILE 'C:/Users/Haseeb/Desktop/roster.csv'
+INTO TABLE Roster
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(NetID, CourseId);*/
+
+/* Table for testiing center information */
 CREATE TABLE TestingCenter (
-	Id INT NOT NULL,
+	Id INT NOT NULL AUTO_INCREMENT,
 	NumberOfSeats INT,
 	NumberofSetAsideSeats INT,
 	GapTime INT,
@@ -40,6 +77,8 @@ CREATE TABLE TestingCenter (
 	PRIMARY KEY (Id)
 );
 
+/* Table for testing center hours of operation.
+Since the hours can change on a day-by-day basis, each day of the term is a tuple in the table */
 CREATE TABLE TestingCenterHours (
 	TestingCenterId INT NOT NULL,
 	OpenDate DATE,
@@ -51,6 +90,7 @@ CREATE TABLE TestingCenterHours (
 		ON DELETE CASCADE
 );
 
+/* Table for the dates in which the testing center is closed */
 CREATE TABLE ClosedDates (
 	TestingCenterId INT NOT NULL,
 	ClosedDate DATE,
@@ -60,6 +100,7 @@ CREATE TABLE ClosedDates (
 		ON DELETE CASCADE
 );
 
+/* Table for exam information */
 CREATE TABLE Exam (
 	Id CHAR(15) NOT NULL,
 	ExamType CHAR(6),
@@ -79,6 +120,7 @@ CREATE TABLE Exam (
 		REFERENCES TestingCenter (Id)
 );
 
+/* Table for course exam information */
 CREATE TABLE CourseExam (
 	ExamId CHAR(15) NOT NULL,
 	CourseId CHAR(10) NOT NULL,
@@ -92,6 +134,8 @@ CREATE TABLE CourseExam (
 		ON DELETE CASCADE
 );
 
+/* Table for Ad-Hoc Exam information.
+Since Ad-Hoc Exams require a list of students, each student is a tuple in this table */
 CREATE TABLE AdHocExam (
 	ExamId CHAR(15) NOT NULL,
 	StudentNetId VARCHAR(20) NOT NULL,
@@ -104,8 +148,9 @@ CREATE TABLE AdHocExam (
 		ON DELETE CASCADE
 );
 
+/* Table for Appointment information */
 CREATE TABLE Appointment (
-	Id INT NOT NULL,
+	Id INT NOT NULL AUTO_INCREMENT,
 	StudentNetId VARCHAR(20),
 	AppointmentDate DATETIME,
 	ExamId CHAR(15),
