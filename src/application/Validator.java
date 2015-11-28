18b8e12fb62754978de2c9fd344a7f0b43b98f06
Haseeb Shahid <hshahid92@gmail.com>
2015-11-27 21:45:00 -0500

    There we have it, boys. The most complete and sexiest schedulability test to date.

--- src/application/Validator.java
+++ src/application/Validator.java
@@ -1,14 +1,18 @@
 package application;
 
+import jpaentities.Appointment;
 import jpaentities.Exam;
 import jpaentities.TestingCenter;
-import sun.reflect.annotation.ExceptionProxy;
+import jpaentities.TestingCenterHour;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Haseeb on 11/21/2015.
@@ -120,32 +124,61 @@ public class Validator {
     }
 
     /**
-     * Checks to see if there are enough seats in the testing center to hold the exam
-     * @param examStart the exam start date
-     * @param examEnd the exam end date
-     * @param numberOfStudents the number of students scheduled to take the exam
-     * @return true if the testing center has enough seats to hold the exam, false otherwise
+     * Checks to see if the testing center has enough time to hold the exam
+     * @param examStart exam start date
+     * @param examEnd exam end date
+     * @param numberOfStudents number of students needing to take the exam
+     * @param duration the duration of exam IN MINUTES
+     * @return true if there is enough time to schedule the exam, false otherwise
      */
-    public boolean isSchedulable(Date examStart, Date examEnd, int numberOfStudents) {
-        LocalDate start = examStart.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        LocalDate end = examEnd.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        int daysActive = (int) ChronoUnit.DAYS.between(start, end);
+    public boolean isSchedulable(Date examStart, Date examEnd, int numberOfStudents, int duration) {
+        LocalDateTime start = examStart.toInstant().atZone(ZoneId.systemDefault()).toLocalDate().atStartOfDay();
+        LocalDateTime end = examEnd.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        Retriever retriever = Retriever.getInstance();
+
+        // FIRST GET THE TOTAL NUMBER OF MINUTES THE TESTING CENTER HAS AVAILABLE IN THE DATE RANGE
+        int totalTestingCenterMinutes = 0;
+        LocalTime hourStart;
+        LocalTime hourEnd;
+        for (LocalDateTime date = start; date.isBefore(end); date = date.plusDays(1)) {
+            Date convertedDate = Date.from(date.atZone(ZoneId.systemDefault()).toInstant());
+            TestingCenterHour tch = retriever.getTestingCenterHour(convertedDate);
+            if (tch != null) {
+                hourStart = tch.getStartTime().toLocalTime();
+                hourEnd = tch.getEndTime().toLocalTime();
+                totalTestingCenterMinutes += (int) ChronoUnit.MINUTES.between(hourStart, hourEnd);
+            }
+            else return false;
+        }
 
         TestingCenter testingCenter = Retriever.getTestingCenter();
-        int totalSeats = daysActive * (testingCenter.getNumberOfSeats() - testingCenter.getNumberOfSetAsideSeats());
+        totalTestingCenterMinutes *= testingCenter.getNumberOfSeats() - testingCenter.getNumberOfSetAsideSeats();
 
-        long seatsUnavailable;
+        // SECOND GET THE TOTAL NUMBER OF MINUTES THAT BEING TAKEN UP BY EXISTING APPOINTMENTS IN THE DATE RANGE
+        List<Appointment> appointments;
         try {
-            query = em.createQuery("SELECT COUNT(a) FROM Appointment a WHERE a.appointmentStatus <> 'CANCELLED' AND a.startDate BETWEEN ?1 and ?2");
+            query = em.createQuery("SELECT a FROM Appointment a WHERE a.appointmentStatus <> 'CANCELLED' AND a.startDate BETWEEN ?1 and ?2");
             query.setParameter(1, examStart, TemporalType.TIMESTAMP);
             query.setParameter(2, examEnd, TemporalType.TIMESTAMP);
-            seatsUnavailable = (long) query.getSingleResult();
+            appointments = query.getResultList();
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
 
-        return numberOfStudents <= totalSeats - (int) seatsUnavailable;
+        int totalApptDuration = 0;
+        LocalDate apptStart;
+        LocalDate apptEnd;
+        for (Appointment appointment : appointments) {
+            apptStart = appointment.getStartDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            apptEnd = appointment.getEndDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            totalApptDuration += (int) ChronoUnit.MINUTES.between(apptStart, apptEnd);
+        }
+
+        // THIRD THE TOTAL NUMBER OF MINUTES THE EXAM NEEDS FOR ALL ITS STUDENTS
+        int totalExamDuration = numberOfStudents * duration;
+
+        return totalExamDuration <= totalTestingCenterMinutes - totalApptDuration;
     }
 
 }
